b4a27313adbb3c6086ab2017af0d36c2bec06df3
ivVane <iv.vane@gmail.com>
2021-10-01 22:17:26 +0200

    Starting point of the Data Binding In Android code lab

--- app/src/main/java/com/vane/android/databindinginandroid/PlainActivity.kt
+++ app/src/main/java/com/vane/android/databindinginandroid/PlainActivity.kt
@@ -1,11 +1,81 @@
 package com.vane.android.databindinginandroid
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
+import androidx.lifecycle.ViewModelProviders
+import com.vane.android.databindinginandroid.data.Popularity
+import com.vane.android.databindinginandroid.data.SimpleViewModel
 
 class PlainActivity : AppCompatActivity() {
+
+    // Obtain ViewModel from ViewModelProviders
+    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plain)
+
+        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
+        updateName()
+        updateLikes()
+    }
+
+    /**
+     * So much findViewById! We'll fix that with Data Binding.
+     */
+    private fun updateName() {
+        findViewById<TextView>(R.id.plain_name).text = viewModel.name
+        findViewById<TextView>(R.id.plain_lastname).text = viewModel.lastName
+    }
+
+    /**
+     * This method has many problems:
+     * - It's calling findViewById multiple times
+     * - It has untestable logic
+     * - It's updating two views when called even if they're not changing
+     */
+    private fun updateLikes() {
+        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
+        findViewById<ProgressBar>(R.id.progressBar).progress =
+            (viewModel.likes * 100 / 5).coerceAtMost(100)
+        val image = findViewById<ImageView>(R.id.imageView)
+
+        val color = getAssociatedColor(viewModel.popularity, this)
+
+        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
+
+        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
+    }
+
+    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+        return when (popularity) {
+            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
+                intArrayOf(android.R.attr.colorForeground)
+            ).getColor(0, 0x000000)
+            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+        }
+    }
+
+    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
+        return when (popularity) {
+            Popularity.NORMAL -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
+            }
+            Popularity.POPULAR -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+            }
+            Popularity.STAR -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+            }
+        }
     }
 }
